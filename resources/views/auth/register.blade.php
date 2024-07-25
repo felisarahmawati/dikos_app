abdc93af4142d537e8f4ac82e00fe35c050ee55c
Felisa Rahmawati <felisarahmawati6@gmail.com>
2024-07-25 09:08:50 +0700

    revisi register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,18 +11,21 @@
             <div class="card shadow mx-auto" style="max-width: 400px;"> <!-- Menambahkan max-width -->
                 <div class="card-body">
                     <div class="text-center">
-                        <img src="assets/img/logo_app.png" alt="logo" width="100" class="mb-3">
+                        <img src="{{ asset('assets/img/logo_app.png') }}" alt="logo" width="100" class="mb-3">
                         <h4 class="mb-4">Register</h4>
                     </div>
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label">{{ __('Name') }}</label>
-
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -33,10 +36,8 @@
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label">{{ __('Email') }}</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -47,10 +48,8 @@
 
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
-
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -61,7 +60,6 @@
 
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label">{{ __('Confirm Password') }}</label>
-
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
